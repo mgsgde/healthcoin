5566b96b2a277c7792a48d96ce52a159b27de5a1
mgsgde <mgsgde@gmail.com>
2018-12-18 18:33:55 +0100

    edited readme

--- nodejs.server/config.js
+++ nodejs.server/config.js
@@ -11,19 +11,19 @@ const PRODUCTION = {
   },
 
   credentials: {
-    infuraApiKey: "c8df1ea6bba245b1a61f5bbdbd3cc14a"
+    infuraApiKey: process.env.INFURAAPIKEY
   },
 
   accounts: {
 
     sender: {
       address: "0xc2c5a236395382f861a3d956c720b29a4ae4f15b",
-      privateKey: "0xa78a0c60210d7ffa2fa3f75de66c16807c9a21879b543aa1e9d17c9065460c83",
+      privateKey: process.env.SENDERPRIVATEKEY,
     },
 
     receiver: {
       address: "0x29153c959be8fea3b08e7be8594dcbf19366ce8f",
-      privateKey: "0x59f30428e71675b8e8cfb7dea06434b8b95f802cbf2b9bc80d7d0b05807843d2",
+      privateKey: process.env.RECEIVERPRIVATEKEY,
     }
   }
 
