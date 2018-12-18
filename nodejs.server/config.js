b051fa7771ce250dcf0ee1271ada4aa5164f785a
mgsgde <mgsgde@gmail.com>
2018-12-18 18:16:22 +0100

    added code

--- nodejs.server/config.js
+++ nodejs.server/config.js
@@ -11,19 +11,19 @@ const PRODUCTION = {
   },
 
   credentials: {
-    infuraApiKey: ""
+    infuraApiKey: "c8df1ea6bba245b1a61f5bbdbd3cc14a"
   },
 
   accounts: {
 
     sender: {
-      address: "",
-      privateKey: "",
+      address: "0xc2c5a236395382f861a3d956c720b29a4ae4f15b",
+      privateKey: "0xa78a0c60210d7ffa2fa3f75de66c16807c9a21879b543aa1e9d17c9065460c83",
     },
 
     receiver: {
-      address: "",
-      privateKey: "",
+      address: "0x29153c959be8fea3b08e7be8594dcbf19366ce8f",
+      privateKey: "0x59f30428e71675b8e8cfb7dea06434b8b95f802cbf2b9bc80d7d0b05807843d2",
     }
   }
 
